55d9b8b1903bcfd062fc5a1ee8b0d959f22f735f
AtefMMO <mohamedatefimb@gmail.com>
2024-07-28 21:24:09 +0300

    add products done

---
+++ products/add_products.php
@@ -0,0 +1,48 @@
+<?php
+include "../connect.php";
+include "../core.php";
+$response = [];
+try {
+    $name = secureRequest($_POST['name']);
+    $price = secureRequest($_POST['price']);
+    $description = secureRequest($_POST['description']);
+    $image = secureRequest($_POST['image']);
+    $category = secureRequest($_POST['category']);
+    $color = secureRequest($_POST['color']);
+    $sale = secureRequest($_POST['sale']);
+    $quantity = secureRequest($_POST['quantity']);
+    $availability = secureRequest($_POST['availability']);
+    $reference = secureRequest($_POST['reference']);
+    $stmt = $connection->prepare("INSERT INTO `products`(`name`,`price`,`description`,`image`,`category`,`color`,`sale`,`quantity`,`availability`,`reference`) VALUES(?,?,?,?,?,?,?,?,?,?)");
+    $stmt->execute(array(
+        $name,
+        $price,
+        $description,
+        $image,
+        $category,
+        $color,
+        $sale,
+        $quantity,
+        $availability,
+        $reference
+    ));
+    $response['status'] = "success";
+    $response["message"] = "Product Added Successfully";
+    $response["product"] = [
+        "name" => $name,
+        "price" => $price,
+        "description" => $description,
+        "image" => $image,
+        "category" => $category,
+        "color" => $color,
+        "sale" => $sale,
+        "quantity" => $quantity,
+        "availability" => $availability,
+        "reference" => $reference
+    ];
+    
+} catch (PDOException $e) {
+    $response['status'] = "error";
+    $response["message"] = "Error: " . $e->getMessage();
+}
+echo json_encode($response);
